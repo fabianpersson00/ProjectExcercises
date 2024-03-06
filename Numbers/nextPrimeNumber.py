8380ca2d3507c7c85e82bcb7f71cb6b54d77c8f4
fabianpersson00 <fnpersson@outlook.com>
2024-03-06 15:38:39 +0100

    Create nextPrimeNumber.py
    Created program to find the next prime number in linear order given whether the user wants to continue or not.

---
+++ Numbers/nextPrimeNumber.py
@@ -0,0 +1,28 @@
+"""
+**Next Prime Number** - Have the program find prime numbers until the user chooses to stop asking for the next one.
+"""
+
+print("Start by typing any character(s) and pressing 'Enter' to generate Prime Number starting from 2. For next prime type 'next', or if 'stop' to stop the program.")
+input()
+
+num = 2
+divBy = 2
+printPrime = True
+while printPrime:
+    if num%divBy == 0 and num == divBy:
+        print("Prime: ", num)
+        print("Do you wish to continue? Write 'next' or 'stop'")
+        toContinue = str(input())
+        if toContinue == "stop":
+            printPrime = False
+        else:
+            num += 1
+            divBy = 2
+    elif divBy < num:
+        if num % divBy == 0:
+            num += 1
+            divBy = 2
+        else:
+            divBy += 1
+    else:
+        print("Wtf")
