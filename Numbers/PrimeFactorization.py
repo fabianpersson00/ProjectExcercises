9e7b58837fd7df6de38258c8a969b5bf8e9367fe
fabianpersson00 <fnpersson@outlook.com>
2024-03-06 15:38:10 +0100

    Refined PrimeFactorization
    Refine to keep everything recursive inside the loop.

--- Numbers/PrimeFactorization.py
+++ Numbers/PrimeFactorization.py
@@ -1,20 +1,22 @@
 """
 **Prime Factorization** - Have the user enter a number and find all Prime Factors (if there are any) and display them.
 """
-
 InputNumber = int(input())
-ListofPrimeFactors = []
 
-def CheckForPrime(IntToCheck):
+def CheckForPrime(IntToCheck, DivBy = 2):
+    ListOfPrimes = []
+    IntToCheck
     if IntToCheck == 0 or IntToCheck == 1:
         print("0 and 1 are not factorial numbers")
+        return
     else:
-        for x in range(2, int(IntToCheck+1)):
-            if (IntToCheck % x) == 0:
-                ListofPrimeFactors.append(x)
-                if (IntToCheck/x) != 1:
-                    CheckForPrime(IntToCheck/x)
-                break
-CheckForPrime(InputNumber)
-if len(ListofPrimeFactors) >= 1:
-    print(ListofPrimeFactors)
+        while (IntToCheck % DivBy == 0):
+            ListOfPrimes.append(DivBy)
+            IntToCheck = IntToCheck/DivBy
+        if (IntToCheck == 1):
+            return ListOfPrimes
+        else:
+            ListOfPrimes = ListOfPrimes + CheckForPrime(IntToCheck, (DivBy+1))
+            return ListOfPrimes
+    
+print(CheckForPrime(InputNumber))
